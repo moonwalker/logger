6be2c0ad7c1cc629df992654743bbdfe2b759e5c
Kalman Speier <kalman.speier@gmail.com>
2020-05-26 16:42:43 +0200

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,14 +1,17 @@
 package logger
 
 type Logger interface {
+	UseTextFormatter()
 	SetLevel(level int)
 	SetContext(context map[string]interface{})
 
 	Println(args ...interface{})
 	Printf(format string, args ...interface{})
-	Info(msg string, fields map[string]interface{})
-	Debug(msg string, fields map[string]interface{})
 
-	Error(msg string, err error)
-	Fatal(msg string, err error)
+	Trace(msg string, fields map[string]interface{})
+	Debug(msg string, fields map[string]interface{})
+	Info(msg string, fields map[string]interface{})
+	Warning(msg string, fields map[string]interface{})
+	Error(msg string, fields map[string]interface{})
+	Fatal(msg string, fields map[string]interface{})
 }
